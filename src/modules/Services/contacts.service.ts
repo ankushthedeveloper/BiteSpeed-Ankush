aa1c79d8317d490228d2741aba483c6a274de98d
Ankush <155977952+ankushthedeveloper@users.noreply.github.com>
2025-02-10 13:13:49 +0530

    Corrected the Contact.Service.ts to work for the Updating to Secondary , Now when we are making the Request ,Current email becomes Primary and Another becomes Secondary , Fixed the Secondary contact Creation as well

--- src/modules/Services/contacts.service.ts
+++ src/modules/Services/contacts.service.ts
@@ -1,8 +1,8 @@
 import { ContactsRepository } from "../Repositories/contacts.repository";
 import { Contact } from "../models/contacts.model";
-
 export class ContactsService {
-  static async identifyOrCreateContact(email?: string, phonenumber?: string) {
+  
+static async identifyOrCreateContact(email?: string, phonenumber?: string) {
     const existingContacts: Contact[] =
       await ContactsRepository.findByEmailOrPhone(email, phonenumber);
 
@@ -30,41 +30,57 @@ export class ContactsService {
     const toConvert: Contact[] = primaryContacts.filter(
       (c) => c.id !== primary.id
     );
+
+    let secondOperation=false;
+
     if (toConvert.length > 0) {
-      await Promise.all(
-        toConvert.map((c) =>
-          ContactsRepository.updateToSecondary(c.id, primary.id)
+     
+     await Promise.all(
+        toConvert.map((c) =>    
+          ContactsRepository.updateToSecondary(primary.id, c.id)
         )
       );
+      secondOperation=true;
+    }
+    const existingContacts2: Contact[] =
+    await ContactsRepository.findByEmailOrPhone(email, phonenumber);
+
+    const primaryContacts2: Contact[] = existingContacts2.filter(
+      (c) => c.linkprecedence === "primary"
+    );
+
+    let primary2: Contact = primaryContacts2.reduce((a, b) =>
+      a.createdat < b.createdat ? a : b
+    );
+
+    const isEmailPresent = existingContacts.some((c) => c.email === email);
+    const isPhonePresent = existingContacts.some((c) => c.phonenumber === phonenumber);
+      
+    
+    if ((!isEmailPresent || !isPhonePresent) && email !== null && phonenumber !== null) {
+      await ContactsRepository.createSecondary(email ?? "", phonenumber ?? "",secondOperation?primary2.id:primary.id);
     }
 
-    const updatedContacts: Contact[] = await ContactsRepository.findByPrimaryId(
-      primary.id
-    );
-
+    
+    const updatedContacts: Contact[] = await ContactsRepository.findByPrimaryId(secondOperation?primary2.id:primary.id);
+    
     const secondaryContacts: Contact[] = updatedContacts.filter(
-      (c) => c.linkedid === primary.id
+      (c) =>  c.linkprecedence==="secondary"
     );
-
     const emails = Array.from(
       new Set(
-        [primary.email, ...secondaryContacts.map((c) => c.email)].filter(
-          Boolean
-        )
+        [secondOperation?primary2.email:primary.email, ...secondaryContacts.map((c) => c.email)].filter(Boolean)
       )
     );
 
     const phoneNumbers = Array.from(
       new Set(
-        [
-          primary.phonenumber,
-          ...secondaryContacts.map((c) => c.phonenumber),
-        ].filter(Boolean)
+        [secondOperation?primary2.phonenumber:primary.phonenumber, ...secondaryContacts.map((c) => c.phonenumber)].filter(Boolean)
       )
     );
 
     return {
-      primaryContactId: primary.id,
+      primaryContactId: secondOperation?primary2.id:primary.id,
       emails,
       phoneNumbers,
       secondaryContactIds: secondaryContacts.map((c) => c.id),
